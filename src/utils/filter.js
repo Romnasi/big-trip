b7bff567c9ba15b9773ae4fdca4dcac2d91a96a9
Roman Nasyrov <romnasi@gmail.com>
2021-05-31 00:35:02 +0300

    Исправлеят замечание по Б1: реализует 'точки маршрута, у которых дата начала меньше текущей даты, а дата окончания — больше, отображаются во всех трёх списках'

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -4,6 +4,10 @@ import {FilterType} from './../const.js';
 
 export const filter = {
   [FilterType.EVERYTHING]: (points) => points.filter((point) => point),
-  [FilterType.FUTURE]: (points) => points.filter((point) => point.date.dateTo > dayjs()),
-  [FilterType.PAST]: (points) => points.filter((point) => point.date.dateFrom < dayjs()),
+  [FilterType.FUTURE]: (points) => points.filter((point) => {
+    return point.date.dateTo > dayjs() || point.date.dateTo < dayjs() && point.date.dateFrom > dayjs();
+  }),
+  [FilterType.PAST]: (points) => points.filter((point) => {
+    return point.date.dateFrom < dayjs() || point.date.dateTo < dayjs() && point.date.dateFrom > dayjs();
+  }),
 };
