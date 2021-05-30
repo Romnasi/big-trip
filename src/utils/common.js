56b7f86046bfcfff10e891428fff973c5aafa316
Roman Nasyrov <romnasi@gmail.com>
2021-05-31 00:42:07 +0300

    Исправлеят замечание по Б22: Используются строгие сравнения вместо нестрогих

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,3 @@
-export const getDescByCity = (city, destinations) => destinations.find((destination) => destination.name == city).description;
-export const getPhotosByCity = (city, destinations) => destinations.find((destination) => destination.name == city).pictures;
+export const getDescByCity = (city, destinations) => destinations.find((destination) => destination.name === city).description;
+export const getPhotosByCity = (city, destinations) => destinations.find((destination) => destination.name === city).pictures;
 export const getQuotientWithoutRemainder = (val, by) => (val - val % by) / by;
